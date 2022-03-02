0dc480aaf24022e9e457fedf22f4596369b6bd57
Will Pirnasch <VisualStudioCE@pirnasch.com>
2022-03-02 00:08:45 -0500

    2019 day 6 B

--- AdventofCode/Source/2019 Solutions/AoC2019_06.h
+++ AdventofCode/Source/2019 Solutions/AoC2019_06.h
@@ -1,5 +1,4 @@
-
-template<> auto InputData<2019, 6, A>() { return "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L"; };
+//template<> auto InputData<2019, 6, A>() { return "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L"; };
 template<> Number AoC<2019, 6, A>(std::istream& input)
 {
   auto list = ReadWords(input);
@@ -53,6 +52,8 @@ template<> Number AoC<2019, 6, A>(std::istream& input)
   return z;
 }
 
+
+//template<> auto InputData<2019, 6, B>() { return "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN"; };
 template<> Number AoC<2019, 6, B>(std::istream& input)
 {
   auto list = ReadWords(input);
@@ -95,12 +96,35 @@ template<> Number AoC<2019, 6, B>(std::istream& input)
     return entry.level + 1;
   };
 
-  Number z{ 0 };
   for (auto& entry : orbits)
   {
     if (entry.second.level == noLevel)
       getLevel(getLevel, entry.second);
-    z += entry.second.level;
+  }
+
+  auto oSAN = orbits.find("SAN");  // Santa
+  assert(oSAN != orbits.end());
+  auto oYOU = orbits.find("YOU");  // You
+  assert(oYOU != orbits.end());
+
+  
+  auto Transfer = [&orbits](auto& o) { o = orbits.find(o->second.center); assert(o != orbits.end()); };
+
+  Number z{ 0 };
+  while (oSAN->second.center != oYOU->second.center)
+  {
+    bool transferSAN = oSAN->second.level >= oYOU->second.level;
+    bool transferYOU = oYOU->second.level >= oSAN->second.level;
+    if (transferSAN)
+    {
+      Transfer(oSAN);
+      ++z;
+    }
+    if (transferYOU)
+    {
+      Transfer(oYOU);
+      ++z;
+    }
   }
 
   return z;
