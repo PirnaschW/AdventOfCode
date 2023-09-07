82aafa149d6686a71a8f58f33efe0413cae230dc
PirnaschW <github@pirnasch.com>
2023-09-07 17:56:45 -0600

    solution for event 2015 day 1; adjusted all Visual Studio output directories

--- AdventofCode/Source/2015 Solutions/AoC2015_01.h
+++ AdventofCode/Source/2015 Solutions/AoC2015_01.h
@@ -4,7 +4,17 @@ template<> Number AoC<2015, 1, A>(std::istream& input)
   using Input = std::vector<std::string>;
   Input v = ReadLines(input);  // read all lines into vector
 
-  return 0;
+  Number res{ 0 };
+  for (const auto& c : v[0])
+  {
+    switch (c)
+    {
+      case '(': ++res; break;
+      case ')': --res; break;
+      default: assert(false);
+    }
+  }
+  return res;
 }
 
 //template<> auto InputData<2015, 1, B>() { return "Testdata\n";}; // Result: ?
@@ -12,5 +22,17 @@ template<> Number AoC<2015, 1, B>(std::istream& input)
 {
   using Input = std::vector<std::string>;
   Input v = ReadLines(input);  // read all lines into vector
-  return 0;
+
+  int res{ 0 };
+  for (int i = 0; i < v[0].size(); ++i)
+  {
+    switch (v[0][i])
+    {
+      case '(': ++res; break;
+      case ')': --res; break;
+      default: assert(false);
+    }
+    if (res < 0) return i + 1;
+  }
+  return -1;
 }
